44ca0199ed769f588ca584a1da587793a389fe44
Elton Chou <plscd748@gmail.com>
2019-03-11 14:28:16 +0800

    uhhhhh....

--- xivapi/core.py
+++ xivapi/core.py
@@ -1,3 +1,4 @@
+import aiohttp
 import requests
 
 
@@ -5,14 +6,20 @@ class Core:
 
     __API_BASE = 'https://xivapi.com/'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, test_mode):
         self.api_key = api_key
+        self.test_mode = test_mode
 
     def get(self, endpoint, params=None):
         if not params:
             params = {}
         params['key'] = self.api_key
         url = self.request_url(endpoint)
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(url) as r:
+        #         if self.test_mode:
+        #             return await r
+        #         return await r.json()
         r = requests.get(url, params)
         if self.test_mode:
             return r
